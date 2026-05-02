2270c106fe77016f0bed35c6288b1a5120e4924f
Sneha Jadhav <135454332+sj1304@users.noreply.github.com>
2026-05-02 23:12:59 +0530

    Update App.js

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import { useState } from "react";
 import './App.css';
 import Intro from './Components/Intro.js';
 import Nav from './Components/Nav.js';
+import Github from './Components/GitHub.js';
 import Projects from './Components/Projects.js';
 import Skills from './Components/Skills.js';
 import bgVideo from "./images/dark-theme-bg.mp4";
@@ -9,13 +10,23 @@ function App() {
 
   const [theme, setTheme] = useState("dark");
 
-  return (
-    <div className='container' id={theme}>
+  return (<>
+  
     {theme === "dark" && (
-        <video autoPlay loop muted playsInline className="bg-video">
+        <video
+  autoPlay
+  loop
+  muted
+  playsInline
+  className="bg-video"
+  ref={(video) => {
+    if (video) video.playbackRate = 0.4; // 🔥 0.5 = half speed
+  }}>
           <source src={bgVideo} type="video/mp4" />
         </video>
       )}
+    <div className='container' id={theme}>
+    
       <Nav theme={theme} setTheme={setTheme} />
 
       <section className='real-content'>
@@ -31,9 +42,12 @@ function App() {
         <div id="projects">
           <Projects theme={theme} />
         </div>
-
+        <div id="github">
+          <Github />
+        </div>
       </section>
     </div>
+    </>
   );
 }
 
